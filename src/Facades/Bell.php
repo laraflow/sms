20d64bbdaabd5aa51a3047f239f524c7c6117955
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-11-04 14:46:41 +0600

    facade class doc support added

--- src/Facades/Bell.php
+++ src/Facades/Bell.php
@@ -5,6 +5,8 @@ namespace Fintech\Bell\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * // Crud Service Method Point Do not Remove //
+ *
  * @see \Fintech\Bell\Bell
  */
 class Bell extends Facade
