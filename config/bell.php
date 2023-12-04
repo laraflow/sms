a9a3d70b31cbdd18060590eb2489baf980e985c3
hafijul233 <hafijul233@users.noreply.github.com>
2023-12-04 08:06:27 +0000

    Fix styling

--- config/bell.php
+++ config/bell.php
@@ -48,7 +48,7 @@ return [
             'sandbox' => [
                 'url' => 'https://rest.clicksend.com/v3/sms/send',
                 'username' => 'masud@clavisint.com',
-//                                'password' => 'Masudalam@13119214',
+                //                                'password' => 'Masudalam@13119214',
                 'password' => 'D08ECA95-5C9B-B77B-D6B9-47AF3CED3F5E',
                 'from' => null,
             ],
