24c60b1b4d72b0c040b10b7351b0deda92e88d62
hafijul233 <hafijul233@users.noreply.github.com>
2023-12-25 20:10:57 +0000

    Fix styling

--- src/Http/Requests/UpdateTriggerVariableRequest.php
+++ src/Http/Requests/UpdateTriggerVariableRequest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateTriggerVariableRequest extends FormRequest
 {
-
     /**
      * Determine if the user is authorized to make this request.
      */
