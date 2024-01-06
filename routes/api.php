b16c9e963c92d3537c4af66998250641ab69477b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-01-06 22:50:51 +0600

    route long line refactored

--- routes/api.php
+++ routes/api.php
@@ -19,22 +19,34 @@ if (Config::get('fintech.bell.enabled')) {
         ->group(function () {
 
             Route::apiResource('triggers', \Fintech\Bell\Http\Controllers\TriggerController::class);
-            Route::post('triggers/{trigger}/restore', [\Fintech\Bell\Http\Controllers\TriggerController::class, 'restore'])->name('triggers.restore');
+            Route::post('triggers/{trigger}/restore',
+                [\Fintech\Bell\Http\Controllers\TriggerController::class, 'restore'])
+                ->name('triggers.restore');
 
             Route::apiResource('triggers', \Fintech\Bell\Http\Controllers\TriggerController::class);
-            Route::post('triggers/{trigger}/restore', [\Fintech\Bell\Http\Controllers\TriggerController::class, 'restore'])->name('triggers.restore');
+            Route::post('triggers/{trigger}/restore',
+                [\Fintech\Bell\Http\Controllers\TriggerController::class, 'restore'])
+                ->name('triggers.restore');
 
             Route::apiResource('trigger-recipients', \Fintech\Bell\Http\Controllers\TriggerRecipientController::class);
-            Route::post('trigger-recipients/{trigger_recipient}/restore', [\Fintech\Bell\Http\Controllers\TriggerRecipientController::class, 'restore'])->name('trigger-recipients.restore');
+            Route::post('trigger-recipients/{trigger_recipient}/restore',
+                [\Fintech\Bell\Http\Controllers\TriggerRecipientController::class, 'restore'])
+                ->name('trigger-recipients.restore');
 
             Route::apiResource('trigger-variables', \Fintech\Bell\Http\Controllers\TriggerVariableController::class);
-            Route::post('trigger-variables/{trigger_variable}/restore', [\Fintech\Bell\Http\Controllers\TriggerVariableController::class, 'restore'])->name('trigger-variables.restore');
+            Route::post('trigger-variables/{trigger_variable}/restore',
+                [\Fintech\Bell\Http\Controllers\TriggerVariableController::class, 'restore'])
+                ->name('trigger-variables.restore');
 
             Route::apiResource('notification-templates', \Fintech\Bell\Http\Controllers\NotificationTemplateController::class);
-            Route::post('notification-templates/{notification_template}/restore', [\Fintech\Bell\Http\Controllers\NotificationTemplateController::class, 'restore'])->name('notification-templates.restore');
+            Route::post('notification-templates/{notification_template}/restore',
+                [\Fintech\Bell\Http\Controllers\NotificationTemplateController::class, 'restore'])
+                ->name('notification-templates.restore');
 
             Route::apiResource('trigger-actions', \Fintech\Bell\Http\Controllers\TriggerActionController::class);
-            Route::post('trigger-actions/{trigger_action}/restore', [\Fintech\Bell\Http\Controllers\TriggerActionController::class, 'restore'])->name('trigger-actions.restore');
+            Route::post('trigger-actions/{trigger_action}/restore',
+                [\Fintech\Bell\Http\Controllers\TriggerActionController::class, 'restore'])
+                ->name('trigger-actions.restore');
 
             //DO NOT REMOVE THIS LINE//
         });
