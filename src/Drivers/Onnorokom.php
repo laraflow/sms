7fcf74aef56ea2c72850cd480b8dcbcf30e47387
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-04-09 00:18:56 +0600

    bangladesh driver added

---
+++ src/Drivers/Onnorokom.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Laraflow\Sms\Drivers;
+
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Laraflow\Sms\Contracts\SmsDriver;
+use Laraflow\Sms\SmsMessage;
+
+/**
+ * @reference
+ */
+class Onnorokom extends SmsDriver
+{
+    /**
+     * Return validation rules for
+     * that sms driver to operate.
+     */
+    public function rules(): array
+    {
+        return [
+            'url' => 'required|url:http,https',
+            'apiKey' => 'required|string',
+            'username' => 'required|string',
+        ];
+    }
+
+    /**
+     * Execute the sms sending request to api provider
+     *
+     * @param SmsMessage $message
+     * @return Response
+     */
+    public function send(SmsMessage $message): Response
+    {
+        $this->payload = [
+            'username' => $this->config['username'],
+            'to' => $message->getReceiver(),
+            'from' => $message->getSender(),
+            'message' => $message->getContent(),
+        ];
+
+        $this->removeEmptyParams();
+
+        return Http::withoutVerifying()
+            ->timeout(30)
+            ->withHeader('Content-Type', 'application/json')
+            ->withHeader('Accept', 'application/json')
+            ->withHeader('apiKey', $this->config['apiKey'])
+            ->get($this->config['url'], $this->payload);
+
+    }
+}
