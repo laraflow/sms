32e6db161d4465d30bf5fb91efb8f3137038e107
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-07-07 18:54:12 +0600

    vendors name config typo fixed

--- src/SmsChannel.php
+++ src/SmsChannel.php
@@ -36,7 +36,7 @@ class SmsChannel
             throw new InvalidArgumentException('No SMS vendor driver configured.');
         }
 
-        $driverClass = config("sms.vendors.{$active}.driver");
+        $driverClass = config("sms.providers.{$active}.driver");
 
         if ($driverClass == null || !class_exists($driverClass)) {
             throw new DriverNotFoundException("No driver configuration found by `{$active}` name.");
@@ -46,7 +46,7 @@ class SmsChannel
 
         $mode = config('sms.mode', 'sandbox');
 
-        $config = config("sms.vendors.{$active}.{$mode}", []);
+        $config = config("sms.providers.{$active}.{$mode}", []);
 
         $this->driver = App::make($driverClass);
 
